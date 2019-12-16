22064c03ec0e59b55a13c11160836cd073c39144
zerochen2016 <470133773@qq.com>
2019-12-16 23:37:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jc-live-android
---
+++ app/src/main/java/jc/live/HomeActivity.java
@@ -0,0 +1,14 @@
+package jc.live;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
